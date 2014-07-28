f2beead55911eb95a29f8077687ecd374c288c8a
Joe Dowdell <joedowdell@gmail.com>
2014-07-28 17:57:23 +0100

    Adding tags to links

--- server.rb
+++ server.rb
@@ -7,6 +7,7 @@ env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
+require './lib/tag'
 
 DataMapper.finalize
 
@@ -26,9 +27,13 @@ set :public_dir, Proc.new { File.join(root, "public") }
   post '/links' do
   	url = params["url"]
   	title = params["title"]
-  	Link.create(:url => url, :title => title)
+    tags = params["tags"].split(" ").map do |tag|
+      Tag.first_or_create(:text => tag)
+    end
+    Link.create(:url => url, :title => title, :tags => tags)
   	redirect to('/')
   end 
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -19,5 +19,28 @@ feature "User adds a new link" do
 			click_button 'Add link'
 		end
 	end
-	
 end
+
+feature "Users can add tags to links" do
+
+	scenario "with a few tags" do
+		visit "/"
+		add_link("http://www.makersacademy.com",
+							"Makers Academy",
+							['education', 'ruby'])
+		link = Link.first
+		expect(link.tags.map(&:text)).to include("education")
+		expect(link.tags.map(&:text)).to include("ruby")
+	end
+
+	def add_link(url, title, tags = [])
+		within('#new-link') do
+			fill_in 'url', :with => url
+			fill_in 'title', :with => title
+			fill_in 'tags', :with => tags.join(' ')
+			click_button 'Add link'
+		end
+	end
+
+end
+

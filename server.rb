33ecde8788e975e7a4e2ccbb8da72351aaf78765
Joe Dowdell <joedowdell@gmail.com>
2014-07-28 18:25:50 +0100

    Added Filtering by tags

--- server.rb
+++ server.rb
@@ -34,6 +34,12 @@ set :public_dir, Proc.new { File.join(root, "public") }
   	redirect to('/')
   end 
 
+  get '/tags/:text' do
+    tag = Tag.first(:text => params[:text])
+    @links = tag ? tag.links : []
+    erb :index
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0

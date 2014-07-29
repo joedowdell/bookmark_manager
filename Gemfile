01664847fe9057034c658454f1dec4c37ead4a57
Joe Dowdell <joedowdell@gmail.com>
2014-07-29 18:58:54 +0100

    Refactoring

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'rack-flash3'
 gem 'sinatra'
+gem 'sinatra-partial'
 
 group :development, :test do
 	gem 'capybara'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,9 +6,8 @@ ENV["RACK_ENV"] = 'test' # because we need to know what database to work with
 # what environment it's running it: test or development. 
 # The environment determines what database to use.
 
-
-require 'database_cleaner'
 require './app/server'
+require 'database_cleaner'
 require 'capybara/rspec'
 
 Capybara.app = Sinatra::Application

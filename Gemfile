00be5a5299757b0ce8cdcd0577b491d052c5e968
Hannah Waxman <hannah.waxman8@gmail.com>
2014-07-29 11:23:38 +0100

    Adding user accounts

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'bcrpyt-ruby'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'sinatra'
--- server.rb
+++ server.rb
@@ -1,23 +1,33 @@
 require 'sinatra/base'
 require 'data_mapper'
+require './lib/link'
+require './lib/tag'
+require './lib/user'
 
 
 env = ENV["RACK_ENV"] || "development"
 
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
-require './lib/link'
-require './lib/tag'
-
 DataMapper.finalize
 
 DataMapper.auto_upgrade!
 
-
 class BookmarkManager < Sinatra::Base
 
-set :views, Proc.new { File.join(root, "views") }
-set :public_dir, Proc.new { File.join(root, "public") }
+  enable :sessions
+  set :session_secret, 'super secret'
+
+  set :views, Proc.new { File.join(root, "views") }
+  set :public_dir, Proc.new { File.join(root, "public") }
+
+helpers do 
+
+    def current_user
+      @current_user ||= User.get(session[:user_id]) if session[:user_id]
+    end
+
+end 
 
   get '/' do
   	@links = Link.all
@@ -40,6 +50,16 @@ set :public_dir, Proc.new { File.join(root, "public") }
     erb :index
   end
 
+  get '/users/new' do 
+    erb :"users/new"
+  end
+
+  post '/users' do 
+    user = User.create(:email => params[:email],
+                :password => params[:password])
+    session[:user_id] = user.id
+    redirect to('/')
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0

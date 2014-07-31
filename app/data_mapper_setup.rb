581d05190656f0aca1cb1e775340a957ce360597
Joe Dowdell <joedowdell@gmail.com>
2014-07-31 18:29:11 +0100

    added an L to URL, it's needed apparently

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,6 +1,6 @@
 env = ENV["RACK_ENV"] || "development"
  	if ENV["RACK_ENV"] == "production"
- 		DataMapper.setup(:default, ENV["DATABASE_UR"])
+ 		DataMapper.setup(:default, ENV["DATABASE_URL"])
 	else
 		DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
  	end

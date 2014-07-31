dd10fe99b77b0f825f0c64732973d1646fc7a4ac
Joe Dowdell <joedowdell@gmail.com>
2014-07-31 18:19:16 +0100

    added live database url

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,10 +1,16 @@
 env = ENV["RACK_ENV"] || "development"
+ 	if ENV["RACK_ENV"] == "production"
+ 		DataMapper.setup(:default, ENV["DATABASE_UR"])
+	else
+		DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+ 	end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
 
 require './app/models/link'
 require './app/models/tag'
 require './app/models/user'
+
 
 # Run "DataMapper.auto_migrate!" to reboot database
 
